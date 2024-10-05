db85aa8285ef6835cbdcef7d93faffe669cdf088
Jason Phillips <jphil16@icloud.com>
2024-10-04 22:18:45 -0700

    Implement constructor for Motorbike class with properties initialization

--- Develop/src/classes/Motorbike.ts
+++ Develop/src/classes/Motorbike.ts
@@ -14,6 +14,29 @@ class Motorbike extends Vehicle {
   weight: number;
   topSpeed: number;
   wheels: Wheel [];
+
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[]
+  ) {
+    super();
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    this.wheels = wheels;
+  }
+
+  
   
   
   // TODO: Declare properties of the Motorbike class
